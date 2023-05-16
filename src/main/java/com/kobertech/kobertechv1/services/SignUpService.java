b68c20ff06041b721833501f7b47f22fb97c4d34
k.berdelis <k.berdelis@deepsea.ai>
2023-05-16 11:12:54 +0300

    SignUpService interface

---
+++ src/main/java/com/kobertech/kobertechv1/services/SignUpService.java
@@ -0,0 +1,12 @@
+package com.kobertech.kobertechv1.services;
+
+import java.util.Optional;
+
+import com.kobertech.kobertechv1.entities.UserEntity;
+
+public interface SignUpService {
+    UserEntity signUpUser(UserEntity user);
+    UserEntity updateUser(UserEntity user);
+    Optional<UserEntity> getUser();
+    void deleteUser(Long userId);
+}
