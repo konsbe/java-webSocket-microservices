4f13df387afe2d81fbab88c40c30c956a07c9e26
k.berdelis <k.berdelis@deepsea.ai>
2023-05-17 17:56:31 +0300

    UserServiceImplementation pt.2.01 signUp user endpoint

--- src/main/java/com/kobertech/kobertechv1/controllers/UserController.java
+++ src/main/java/com/kobertech/kobertechv1/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.kobertech.kobertechv1.controllers;
 
 import com.kobertech.kobertechv1.entities.UserEntity;
+import com.kobertech.kobertechv1.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/account")
 public class UserController  {
 
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+
+    
     @PostMapping("/signup")
-    public void signUp(@RequestBody UserEntity account) {
+    public ResponseEntity<UserEntity> signUp(@RequestBody UserEntity userEntity) throws Exception {
         // Check if user exists
         // Perform sign-up 
+        return new ResponseEntity<>(userService.signUpUser(userEntity), HttpStatus.OK);
         // Access account.getUserName(), account.getEmail(), account.getPassword(), etc.
         // Save the account to the database or perform any other necessary operations
         // You can also return a response or redirect to a success page
