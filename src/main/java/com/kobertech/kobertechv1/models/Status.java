56d7fdba60e54a94ac3e409ed2617fabee7d8c93
k.berdelis <k.berdelis@deepsea.ai>
2024-02-22 01:08:06 +0200

    feat: call message status

--- src/main/java/com/kobertech/kobertechv1/models/Status.java
+++ src/main/java/com/kobertech/kobertechv1/models/Status.java
@@ -3,5 +3,10 @@ package com.kobertech.kobertechv1.models;
 public enum Status {
     JOIN,
     MESSAGE,
-    LEAVE
+    LEAVE,
+    CALLOFFER,
+    CALLACCEPTED,
+    CANDIDATE,
+    CALLREJECTED,
+    CALLENDED
 }
